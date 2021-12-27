dff4bfb23fb8cac73f33efbf00717139f81cc1ec
SamuelSteven <samuel.steven880@gmail.com>
2021-12-27 23:07:34 +0700

    Creating Movies App Home

--- App.js
+++ App.js
@@ -1,28 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import { getPopularMovies } from './services/services';
+import React from 'react';
+import { View } from 'react-native';
+import Home from './screens/Home';
 
-const YourApp = () => {
-  const [movie, setMovie] = useState('');
-  const [error, setError] = useState(false);
-
-  useEffect(() =>{
-    getPopularMovies().then(movies => {
-      setMovie(movies[0]);
-    })
-    .catch(err => {
-      setError(err);
-    });
-  }, []);
- 
+const App = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Movie Name: {movie.original_title}</Text>
-      <Text>Language: {movie.original_language}</Text>
-      <Text>Release Date: {movie.release_date}</Text>
-      {error && <Text style={{color: 'red'}}>Error in the server</Text>}
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Home></Home>
     </View>
   );
-}
+};
 
-export default YourApp;
+export default App;
--- services/services.js
+++ services/services.js
@@ -11,12 +11,29 @@ export const getPopularMovies = async () => {
 
 // Get Upcoming Movies
 export const getUpcomingMovies = async () => {
-    const resp = await axios.get(`${apiUrl}/movie/upcoming?${apiKey}`);
-    return resp.data.results;
-  };
+  const resp = await axios.get(`${apiUrl}/movie/upcoming?${apiKey}`);
+  return resp.data.results;
+};
 
 // Get Popular TV
 export const getPopularTv = async () => {
-    const resp = await axios.get(`${apiUrl}/tv/popular?${apiKey}`);
-    return resp.data.results;
-  };
+  const resp = await axios.get(`${apiUrl}/tv/popular?${apiKey}`);
+  return resp.data.results;
+};
+
+// Get Family Movies
+export const getFamilyMovies = async () => {
+  const resp = await axios.get(
+    `${apiUrl}/discover/movie?${apiKey}&with_genres=10751`,
+  );
+  return resp.data.results;
+};
+
+// Get Documnetery Movies
+export const getDocumentaryMovies = async () => {
+  const resp = await axios.get(
+    `${apiUrl}/discover/movie?${apiKey}&with_genres=99`,
+  );
+  return resp.data.results;
+};
+
